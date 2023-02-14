bca63999d5d87df50c758412de6899ebba5086d7
Antonina Vitkalova <tonatonotonya@gmail.com>
2023-02-14 22:28:11 +0700

    displayPlaylist error

--- firebase.js
+++ firebase.js
@@ -1,16 +1,6 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
-import { getStorage, ref } from "https://www.gstatic.com/firebasejs/9.13.0/firebase/storage.js";
+// // import { getStorage } from "firebase/storage";
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.0/firebase-app.js";
+// import {getStorage} from 'https://www.gstatic.com/firebasejs/9.9.0/firebase-storage.js';
 
 
-    const firebaseConfig = {
-        apiKey: "AIzaSyDuFVHuCjPzzMvFm4Km2qtwDdbiqPdFIuA",
-        authDomain: "music-61e15.firebaseapp.com",
-        projectId: "music-61e15",
-        storageBucket: "music-61e15.appspot.com",
-        messagingSenderId: "90519520404",
-        appId: "1:90519520404:web:c12d160c883c1f75ed2081"
-    };
-
-
-    const app = initializeApp(firebaseConfig);
-    const storage = getStorage(app);
+// //firebase
--- index.js
+++ index.js
@@ -1,128 +1,129 @@
 
+fetch('albums.json')
+  .then(response => response.json())
+  .then(albums => {
+
+    // const albumsArr = [album1, album2];
+
+    const albumElements = document.querySelectorAll(".album");
+    const player = document.querySelector("#music-player");
+    const songTitle = document.querySelector("#song-title");
+    const progressBar = document.querySelector("#progress-bar");
+    const progress = document.querySelector("#progress");
+    const searchInput = document.querySelector("#search-input");
+    const searchButton = document.querySelector("#search-button");
+    // const playlist = document.querySelector('.player-container');
 
 
-const albums = [
-  {
-    title: "Aaron",
-    artist: "Hello",
-    songs: [
-      {
-        title: "Aaron",
-        src: "./Aaron.mp3"
-      },
-      {
-        title: "Altes%)",
-        src: "./AltesKamuffel.mp3"
-      },
-    ]
-  },
-  {
-    title: "Altes",
-    artist: "Hello",
-    songs: [
-      {
-        title: "Altes%)",
-        src: "./AltesKamuffel.mp3"
-      },
-      {
-        title: "Aaro",
-        src: "./Aaron.mp3"
-      },
-      { title: "Aaaron", src: "./Aaron.mp3" },
-    ]
-  }
+    let currentAlbum = albums[0];
+    async function displayAlbums() {
+      const response = await fetch('https://jsonplaceholder.typicode.com/albums');
+      const albums = await response.json();
 
-];
+      const albumsContainer = document.getElementById('albums-container');
+      if (!albumsContainer) {
+        console.error('Could not find the albums container element');
+        return;
+      }
 
+      let html = '';
+      albums.forEach((album, index) => {
+        html += `
+          <div class="album" onclick="displayPlaylist(${index})">
+            <p>${album.title}</p>
+          </div>
+        `;
+      });
 
-
-
-// const albumsArr = [album1, album2];
-const albumElements = document.querySelectorAll(".album");
-const player = document.querySelector("#music-player");
-const songTitle = document.querySelector("#song-title");
-const progressBar = document.querySelector("#progress-bar");
-const progress = document.querySelector("#progress");
-const searchInput = document.querySelector("#search-input");
-const searchButton = document.querySelector("#search-button");
-// const playlist = document.querySelector('.player-container');
-
-
-let currentAlbum = albums[0];
-
-const displayAlbums = () => {
-  albumList.innerHTML = "";
-  for (const albumKey in albums) {
-    const album = albums[albumKey];
-    albumList.innerHTML += `
-      <div class="album" data-album="${albumKey}">
-        ${album.title} ${album.artist}
-      </div>
-    `;
-  }
-};
-
-albumElements.forEach((albumElement, index) => {
-
-  albumElement.addEventListener("click", () => {
-    currentAlbum = albums[index];
-    songTitle.innerHTML = currentAlbum.songs[0].title;
-    player.src = currentAlbum.songs[0].src;
-    player.play();
-
-  });
-
-});
-
-player.addEventListener("timeupdate", () => {
-  waveform.style.width = `${(player.currentTime / player.duration) * 100}%`;
-});
-
-waveform.addEventListener("timeupdate", () => {
-  waveform.style.width = `${(waveform.currentTime / waveform.duration) * 100}%`;
-});
+      albumsContainer.innerHTML = html;
+    }
 
 
 
 
-searchButton.addEventListener("click", () => {
-  const searchedAlbum = albums.find(album => album.title.toLowerCase().includes(searchInput.value.toLowerCase()));
-  if (searchedAlbum) {
-    currentAlbum = searchedAlbum;
-    songTitle.innerHTML = currentAlbum.songs[0].title;
-    player.src = currentAlbum.songs[0].src;
-    player.play();
-  }
-});
-
-// Playlist
 
 
-function displayPlaylist(index) {
-  document.querySelectorAll('.playlist').forEach(el => {
-    el.style.display = 'none';
-  });
-  const playlist = document.getElementById(`playlist-${index}`);
-  playlist.style.display = 'block';
-}
-function playSong(src) {
-  document.getElementById('music-player').src = src;
-  document.getElementById('music-player').play();
-}
-albums.forEach((album, index) => {
-  const playlist = document.createElement('div');
-  playlist.classList.add('playlist');
-  playlist.id = `playlist-${index}`;
-  playlist.innerHTML = `
+
+    albumElements.forEach((albumElement, index) => {
+      albumElement.addEventListener("click", () => {
+        currentAlbum = albums[index];
+        songTitle.innerHTML = currentAlbum.songs[0].title;
+        player.src = currentAlbum.songs[0].src;
+        player.play();
+
+      });
+
+    });
+
+    // player.addEventListener("timeupdate", () => {
+    //   waveform.style.width = `${(player.currentTime / player.duration) * 100}%`;
+    // });
+
+    // waveform.addEventListener("timeupdate", () => {
+    //   waveform.style.width = `${(waveform.currentTime / waveform.duration) * 100}%`;
+    // });
+
+
+
+
+    searchButton.addEventListener("click", () => {
+      const searchedAlbum = albums.find(album => album.title.toLowerCase().includes(searchInput.value.toLowerCase()));
+      if (searchedAlbum) {
+        currentAlbum = searchedAlbum;
+        songTitle.innerHTML = currentAlbum.songs[0].title;
+        player.src = currentAlbum.songs[0].src;
+        player.play();
+      }
+    });
+
+
+
+
+    // function playSong(src) {
+    //   document.getElementById('music-player').src = src;
+    //   document.getElementById('music-player').play();
+    // }
+
+
+    albums.forEach((album, index) => {
+      const playlist = document.createElement('div');
+      playlist.classList.add('playlist');
+      playlist.id = `playlist-${index}`;
+      playlist.innerHTML = `
     <h3>${album.title}</h3>
     <ol class="ol-playlist">
     ${album.songs.map(song => `<li class="playsong-li"><a class="playsong" href="#" onclick="playSong('${song.src}')">${song.title}</a> <a class="playsong-artist" href="#">${album.artist}</a></li>`)}
       </ol>`;
-  document.getElementById('playlist').appendChild(playlist);
-});
+
+      playlist.querySelectorAll('.playsong').forEach((playSongElement, songIndex) => {
+        playSongElement.addEventListener('click', (event) => {
+          event.preventDefault();
+          document.getElementById('music-player').src = album.songs[songIndex].src;
+          document.getElementById('music-player').play();
+        });
+      });
+      document.getElementById('playlist').appendChild(playlist);
+    });
+
+
+    function displayPlaylist(index) {
+      const allPlaylists = document.getElementById('playList');
+      for (let i = 0; i < allPlaylists.length; i++) {
+        allPlaylists[i].style.display = 'none';
+      }
+      const selectedPlaylist = document.getElementById(`playList-${index}`);
+      if (selectedPlaylist) {
+        selectedPlaylist.style.display = 'block';
+      }
+  };
 
 
 
 
-// wave
+  });
+
+
+
+
+
 
