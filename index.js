f1b46741d7bec7923b2e2c54649e7d8933665e2f
Antonina Vitkalova <tonatonotonya@gmail.com>
2023-02-21 03:49:15 +0700

    ?

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ fetch('albums.json')
 
     let currentAlbum = albums[0];
 
-    const displayAlbums = () => {
+    const displayAlbums = (albums = []) => {
       albumList.innerHTML = "";
       for (const albumKey in albums) {
         const album = albums[albumKey];
@@ -23,6 +23,7 @@ fetch('albums.json')
       }
     };
 
+
     albumElements.forEach((albumElement, index) => {
       albumElement.addEventListener("click", () => {
         currentAlbum = albums[index];
@@ -33,79 +34,48 @@ fetch('albums.json')
       });
     });
 
-
-    // const displayPlaylist = (index) => {
-    //   playlistElements.forEach((playlist) => {
-    //     playlist.style.display = "none";
-    //   });
-    //   const playlist = playlistElements[index];
-    //   playlist.style.display = "block";
-    // };
     const displayPlaylist = (index) => {
       playlistElements.forEach((playlist) => {
         playlist.style.display = "none";
       });
       const playlist = playlistElements[index];
       playlist.style.display = "block";
-    
-      
+
       const songs = playlist.querySelectorAll(".playsong-link");
       songs.forEach((song) => {
         const playButton = document.createElement("button");
         playButton.classList.add("play-button");
         playButton.innerHTML = '<i class="fas fa-play"></i>';
-    
+
         let isPlaying = false;
-        // let waveformSong;
-    
+
         playButton.addEventListener("click", () => {
           if (!isPlaying) {
-           
             isPlaying = true;
             playButton.innerHTML = '<i class="fas fa-pause"></i>';
             playSong(song.dataset.songTitle, song.dataset.songSrc);
-    
-           
-            // waveformSong = song.parentElement.querySelector(".playsong-waveform");
-            // waveformSong.style.display = "block";
           } else {
-         
             isPlaying = false;
             playButton.innerHTML = '<i class="fas fa-play"></i>';
             pauseSong();
-    
-            
-            // waveformSong.style.display = "none";
           }
         });
-    
-        song.addEventListener("mouseover", () => {
-        
-          song.appendChild(playButton);
+
+        const songLi = song.closest(".playsong-li");
+        songLi.addEventListener("mouseover", () => {
+          // show the play button on hover
+          songLi.appendChild(playButton);
         });
-    
-        song.addEventListener("mouseleave", () => {
-         
-          song.removeChild(playButton);
+
+        songLi.addEventListener("mouseleave", () => {
+          // remove the play button on mouseleave
+          songLi.removeChild(playButton);
         });
       });
+
+
+
     };
-    
-      
-      // const displayWaveform = (waveformSong, isPlaying) => {
-      //   if (isPlaying) {
-      //     waveformSong.style.display = "block";
-      //   } else {
-      //     waveformSong.style.display = "none";
-      //   }
-      // };
-
-    
- 
-    
-    
-
-
 
 
     albums.forEach((album, index) => {
@@ -126,6 +96,7 @@ fetch('albums.json')
         
         </div>
         </div>
+        <div id="waveform" class="playlist-waveform"></div>
         
         
         <ol class="ol-playlist">
@@ -135,6 +106,7 @@ fetch('albums.json')
               `<li class="playsong-li"> 
              
               <div class="playsong-waveform"></div>
+        
 
               <a class="playsong-link" href="#" 
                  data-song-title="${song.title}" 
@@ -153,138 +125,73 @@ fetch('albums.json')
 
       document.getElementById("playlist").appendChild(playlist);
       playlistElements.push(playlist);
+
     });
 
-    player.addEventListener("timeupdate", () => {
-      waveform.style.width = `${(player.currentTime / player.duration) * 100}%`;
-    });
 
-    // waveform.addEventListener("timeupdate", () => {
-    //   waveform.style.width = `${(waveform.currentTime / waveform.duration) * 100}%`;
-    // });
+
 
     const searchInput = document.getElementById("search-input");
-    const searchButton = document.getElementById("search-button");
-
-    searchButton.addEventListener("click", () => {
-      const searchedAlbum = albums.find((album) =>
-        album.name.toLowerCase().includes(searchInput.value.toLowerCase())
+    const searchAlbum = () => {
+      const searchValue = searchInput.value.toLowerCase();
+      const filteredAlbums = albums.filter(album =>
+        album.artist.toLowerCase().includes(searchValue) ||
+        album.name.toLowerCase().includes(searchValue)
       );
 
-      if (searchedAlbum) {
-        currentAlbum = searchedAlbum;
+
+
+      if (filteredAlbums.length > 0) {
+        currentAlbum = filteredAlbums[0];
         songTitle.innerHTML = currentAlbum.songs[0].title;
         player.src = currentAlbum.songs[0].src;
         player.play();
-        displayPlaylist(albums.indexOf(searchedAlbum));
-        playlist.innerHTML = `  <h3 class="album-text__song-title">${currentAlbum.songs[0].title}</h3>`
+        displayPlaylist(albums.indexOf(currentAlbum));
       }
+    };
+    const searchButton = document.getElementById('search-button');
+    searchButton.addEventListener("click", searchAlbum);
+
+
+
+
+  });
+
+
+  let isPlaying = false;
+  function playSong(title, src) {
+    const player = document.querySelector("#music-player");
+    const songTitle = document.querySelector("#song-title");
+    songTitle.innerHTML = title;
+    player.src = src;
+  
+    // get the current playlist
+    const currentPlaylist = document.querySelector(".playlist.show");
+  
+    // get the waveform element for the current song
+    const waveform = currentPlaylist.querySelector("#waveform");
+  
+    player.addEventListener("timeupdate", () => {
+      const progress = player.currentTime / player.duration;
+      waveform.style.width = `${progress * 100}%`;
     });
-
-
-    // const addPlayPauseButton = () => {
-    //   const playButtons = document.querySelectorAll('.playsong-buttons');
-    //   const player = document.querySelector('#music-player');
-    
-    //   if (!player) {
-    //     console.error('Music not found.');
-    //     return;
-    //   }
-    
-    //   if (playButtons) {
-    //     playButtons.forEach((button) => {
-    //       const image = document.createElement("img");
-    //       image.classList.add('equalizer');
-    //       image.setAttribute('src', 'img/equalizer.svg');
-    //       button.appendChild(image);
-    //       image.style.display = "none"; // initially hide the image
-    
-    //       button.addEventListener('click', () => {
-    //         if (player.paused) {
-    //           player.play();
-    //           button.classList.remove('play');
-    //           button.classList.add('pause');
-    //           image.style.display = "block";
-    //         } else {
-    //           player.pause();
-    //           button.classList.remove('pause');
-    //           button.classList.add('play');
-    //           image.style.display = "none";
-    //         }
-    //       });
-    //     });
-    //   }
-    // };
-
-    
-
-    // addPlayPauseButton();
-
-  });
-
-
-
-function playSong(title, src) {
-  const player = document.querySelector("#music-player");
-  const songTitle = document.querySelector("#song-title");
-  songTitle.innerHTML = title;
-  player.src = src;
-  player.addEventListener('canplaythrough', () => {
-    player.play();
-  });
-};
-function pauseSong() {
-  const player = document.querySelector("#music-player");
-  player.pause();
-}
-
-
-// Buttons
-
-
-
-// const playPauseBtn = document.getElementById('play-pause-btn');
-// const player = document.querySelector('#music-player');
-
-// playPauseBtn.addEventListener('click', () => {
-//   if (player.paused) {
-//     player.play();
-//     playPauseBtn.classList.remove('play-btn');
-//     playPauseBtn.classList.add('pause-btn');
-//   } else {
-//     player.pause();
-//     playPauseBtn.classList.remove('pause-btn');
-//     playPauseBtn.classList.add('play-btn');
-//   }
-// });
-
-// player.addEventListener('play', () => {
-//   playPauseBtn.classList.remove('play-btn');
-//   playPauseBtn.classList.add('pause-btn');
-// });
-
-// player.addEventListener('pause', () => {
-//   playPauseBtn.classList.remove('pause-btn');
-//   playPauseBtn.classList.add('play-btn');
-// });
-
-// playPauseBtn.addEventListener("click", function() {
-//   if (player.paused) {
-//     player.play();
-//     playPauseBtn.innerHTML = '<i class="fa fa-pause"></i>';
-//   } else {
-//     player.pause();
-//     playPauseBtn.innerHTML = '<i class="fa fa-play"></i>';
-//   }
-// });
-
-// playPauseBtn.addEventListener("click", () => {
-//   if (player.paused) {
-//     player.play();
-//     playPauseBtn.innerHTML = '<i class="fas fa-pause"></i>';
-//   } else {
-//     player.pause();
-//     playPauseBtn.innerHTML = '<i class="fas fa-play"></i>';
-//   }
-// });
-
+  
+    player.addEventListener("canplaythrough", () => {
+      isPlaying = true;
+      player.play();
+    });
+  }
+  
+  function pauseSong() {
+    const player = document.querySelector("#music-player");
+    const playButton = document.querySelector(".play-button");
+    isPlaying = !isPlaying;
+    if (isPlaying) {
+      player.play();
+      playButton.innerHTML = '<i class="fas fa-pause"></i>';
+    } else {
+      player.pause();
+      playButton.innerHTML = '<i class="fas fa-play"></i>';
+    }
+  }
+  
