16a054cecf73411a72af85a2af38d9f6edf47465
Antonina Vitkalova <tonatonotonya@gmail.com>
2023-02-21 23:48:19 +0700

    css

--- index.js
+++ index.js
@@ -27,13 +27,12 @@ fetch('albums.json')
     albumElements.forEach((albumElement, index) => {
       albumElement.addEventListener("click", () => {
         currentAlbum = albums[index];
-        songTitle.innerHTML = currentAlbum.songs[0].title;
+        // songTitle.innerHTML = currentAlbum.songs[0].title;
         player.src = currentAlbum.songs[0].src;
         player.play();
         displayPlaylist(index);
       });
     });
-
     const displayPlaylist = (index) => {
       playlistElements.forEach((playlist) => {
         playlist.style.display = "none";
@@ -42,22 +41,23 @@ fetch('albums.json')
       playlist.style.display = "block";
 
       const songs = playlist.querySelectorAll(".playsong-link");
+      const player = document.querySelector("#music-player");
+      let isPlaying = false;
+
       songs.forEach((song) => {
         const playButton = document.createElement("button");
         playButton.classList.add("play-button");
         playButton.innerHTML = '<i class="fas fa-play"></i>';
 
-        let isPlaying = false;
-
         playButton.addEventListener("click", () => {
           if (!isPlaying) {
             isPlaying = true;
             playButton.innerHTML = '<i class="fas fa-pause"></i>';
-            playSong(song.dataset.songTitle, song.dataset.songSrc);
+            playSong(song.dataset.songTitle, song.dataset.songSrc, playlist);
           } else {
             isPlaying = false;
             playButton.innerHTML = '<i class="fas fa-play"></i>';
-            pauseSong();
+            pauseSong(player);
           }
         });
 
@@ -72,10 +72,9 @@ fetch('albums.json')
           songLi.removeChild(playButton);
         });
       });
-
-
-
     };
+
+
 
 
     albums.forEach((album, index) => {
@@ -90,10 +89,11 @@ fetch('albums.json')
       <div class="album-info">
       <img class="album-info__cover" src="${album.cover}">
       <div class="album-text">
+      <h3 class="  album-text__song-title">${song.title}</h3>
         <h3 class="album-text__album-name">${album.name}</h3>
         <h3 class="album-text__album-artist">${album.artist}</h3>
         
-        
+      
         </div>
         </div>
         <div id="waveform" class="playlist-waveform"></div>
@@ -104,15 +104,15 @@ fetch('albums.json')
           .map(
             (song) =>
               `<li class="playsong-li"> 
-             
+              
               <div class="playsong-waveform"></div>
         
 
               <a class="playsong-link" href="#" 
-                 data-song-title="${song.title}" 
-                 data-song-src="${song.src}" 
-                 data-song-duration="${song.duration}">
-                 ${song.title}
+                  data-song-title="${song.title}" 
+                  data-song-src="${song.src}" 
+                  data-song-duration="${song.duration}">
+                  ${song.title}
               </a>
               <a class="playsong-artist" href="#">${album.name}</a>
               <span class="playsong-duration">${song.duration}</span>
@@ -125,7 +125,7 @@ fetch('albums.json')
 
       document.getElementById("playlist").appendChild(playlist);
       playlistElements.push(playlist);
-
+      playSong(song.title, song.src, playlist);
     });
 
 
@@ -143,7 +143,7 @@ fetch('albums.json')
 
       if (filteredAlbums.length > 0) {
         currentAlbum = filteredAlbums[0];
-        songTitle.innerHTML = currentAlbum.songs[0].title;
+        // songTitle.innerHTML = currentAlbum.songs[0].title;
         player.src = currentAlbum.songs[0].src;
         player.play();
         displayPlaylist(albums.indexOf(currentAlbum));
@@ -156,42 +156,34 @@ fetch('albums.json')
 
 
   });
+function playSong(title, src, playlist) {
+  const player = document.querySelector("#music-player");
+  const songTitle = title;
+
+  // update the playlist element with the song title
+  playlist.querySelector('.album-text__song-title').textContent = songTitle;
+
+  player.src = src;
+
+  // get the waveform element for the current song
+  const waveform = playlist.querySelector("#waveform");
+
+  player.addEventListener("timeupdate", () => {
+    const progress = player.currentTime / player.duration;
+    waveform.style.width = `${progress * 100}%`;
+  });
+
+  player.addEventListener("canplaythrough", () => {
+    isPlaying = true;
+    player.play();
+  });
+}
+
+function pauseSong() {
+  const player = document.querySelector("#music-player");
+  isPlaying = false; // add this line
+  player.pause();
+}
 
 
-  let isPlaying = false;
-  function playSong(title, src) {
-    const player = document.querySelector("#music-player");
-    const songTitle = document.querySelector("#song-title");
-    songTitle.innerHTML = title;
-    player.src = src;
-  
-    // get the current playlist
-    const currentPlaylist = document.querySelector(".playlist.show");
-  
-    // get the waveform element for the current song
-    const waveform = currentPlaylist.querySelector("#waveform");
-  
-    player.addEventListener("timeupdate", () => {
-      const progress = player.currentTime / player.duration;
-      waveform.style.width = `${progress * 100}%`;
-    });
-  
-    player.addEventListener("canplaythrough", () => {
-      isPlaying = true;
-      player.play();
-    });
-  }
-  
-  function pauseSong() {
-    const player = document.querySelector("#music-player");
-    const playButton = document.querySelector(".play-button");
-    isPlaying = !isPlaying;
-    if (isPlaying) {
-      player.play();
-      playButton.innerHTML = '<i class="fas fa-pause"></i>';
-    } else {
-      player.pause();
-      playButton.innerHTML = '<i class="fas fa-play"></i>';
-    }
-  }
-  
+
